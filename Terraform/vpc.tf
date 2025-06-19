5ca4c5fdd69957f407f0cc9ae0b8a4df21359430
tanyasaini125 <tanya22saini@gmail.com>
2025-06-19 12:55:44 +0530

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "public-subnet" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "public-subnet"
@@ -23,7 +23,7 @@ resource "aws_subnet" "public-subnet" {
 resource "aws_subnet" "private-subnet" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "private-subnet"
